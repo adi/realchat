277dad47e40e1697409fda8fd41bae89f5c33aaf
Adrian Punga <adrian.punga@ejobsgroup.ro>
2020-04-27 00:02:17 +0300

    Clean-up before gRPC switch

--- src/main/java/com/sqreept/realchat/RealChatServer.kt
+++ src/main/java/com/sqreept/realchat/RealChatServer.kt
@@ -1,18 +1,9 @@
 package com.sqreept.realchat
 
-import io.javalin.Javalin
 import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
 
 fun runServer() {
     logger.debug { "Starting server..." }
-    val app = Javalin.create().start("0.0.0.0",3000)
-    app.get("/") { ctx ->
-        logger.warn { "Route / called" }
-        ctx.result("Hello World!")
-    }
-    app.post("/register") { ctx ->
-        ctx.result("{ \"error\": null }")
-    }
 }
